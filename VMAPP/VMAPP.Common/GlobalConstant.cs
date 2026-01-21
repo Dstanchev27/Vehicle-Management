c7d5ad6211074524466c6204345f09b4c42711c3
Dstanchev27 <d.stanchev2007@gmail.com>
2026-01-21 21:43:48 +0200

    Init

---
+++ VMAPP/VMAPP.Common/GlobalConstant.cs
@@ -0,0 +1,12 @@
+﻿namespace VMAPP.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GlobalConstant
+    {
+    }
+}
